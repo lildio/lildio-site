8ccf2b79f519ce5681c1cd7aad288291a7102155
Your Name <you@example.com>
2018-07-26 01:59:57 +0300

    start all new site

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
 from .models import Post
+from .forms import PostForm
 # Create your views here.
 def post_list(request):
     posts = Post.objects.order_by('published_date')
@@ -7,3 +8,15 @@ def post_list(request):
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            #post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
